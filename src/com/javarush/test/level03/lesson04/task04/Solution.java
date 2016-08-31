b9106fc72d1943be206883b1e2dfa3e39a17375b
TetianaFedorenko <fedorenko.tet@gmail.com>
2016-08-31 10:19:44 +0300

    Task4_lesson4

---
+++ src/com/javarush/test/level03/lesson04/task04/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.test.level03.lesson04.task04;
+
+/* Произведение 10 чисел
+Вывести на экран произведение 10 чисел от 1 до 10. Результат - 1 число.
+Подсказка: будет три миллиона с хвостиком.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        int i;
+        int b = 1;
+        for (i = 2; i <= 10; i++) {
+            b = b * i;
+
+        }
+
+        System.out.println(b);
+
+    }
+}
