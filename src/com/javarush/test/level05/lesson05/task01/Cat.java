1322078ca9bf580c2cf0f83cff52c9dd16c5430c
tetiana <test@test.com>
2016-09-10 10:54:35 +0300

    Task1_lesson5_level5

---
+++ src/com/javarush/test/level05/lesson05/task01/Cat.java
@@ -0,0 +1,14 @@
+package com.javarush.test.level05.lesson05.task01;
+
+/* Создать класс Cat
+Создать класс Cat. У кота должно быть имя (name, String), возраст (age, int), вес (weight, int), сила (strength, int).
+*/
+
+public class Cat
+{
+    String name = "Vaska";
+    int age = 6;
+    int weight = 4;
+    int strength = 25;
+
+}
