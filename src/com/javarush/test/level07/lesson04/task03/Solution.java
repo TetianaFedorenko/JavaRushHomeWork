69616f2a26f50686b497a9fb96e7624083ce21b9
tetiana <test@test.com>
2016-09-13 16:37:01 +0300

    Task3_lesson4_level7

---
+++ src/com/javarush/test/level07/lesson04/task03/Solution.java
@@ -0,0 +1,35 @@
+package com.javarush.test.level07.lesson04.task03;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+/* 2 массива
+1. Создать массив на 10 строк.
+2. Создать массив на 10 чисел.
+3. Ввести с клавиатуры 10 строк, заполнить ими массив строк.
+4. В каждую ячейку массива чисел записать длину строки из массива строк, индекс/номер ячейки которой совпадает с текущим индексом из массива чисел.
+Вывести содержимое массива чисел на экран, каждое значение выводить с новой строки.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+        String[] arr = new String[10];
+        int[] arrNum = new int[10];
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < arr.length; i++){
+            String s = reader.readLine();
+            arr[i] = s;
+        }
+
+        for (int i = 0; i < arrNum.length; i++) {
+            arrNum[i] = arr[i].length();
+        }
+
+        for (int i = 0; i < arrNum.length; i++) {
+            System.out.println(arrNum[i]);
+        }
+    }
+}
