7c5d06d0100e02ab001f757b8227af42178c2c90
tetiana <test@test.com>
2016-09-20 14:30:54 +0300

    Bonus1_lesson11_level8

--- src/com/javarush/test/level08/lesson11/bonus01/Solution.java
+++ src/com/javarush/test/level08/lesson11/bonus01/Solution.java
@@ -3,10 +3,7 @@ package com.javarush.test.level08.lesson11.bonus01;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 /* Номер месяца
 Программа вводит с клавиатуры имя месяца и выводит его номер на экран в виде: «May is 5 month».
@@ -17,7 +14,25 @@ public class Solution
 {
     public static void main(String[] args) throws IOException
     {
-        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String monthName = reader.readLine();
+        ArrayList<String> monthList = new ArrayList<>();
+        monthList.add("January");
+        monthList.add("February");
+        monthList.add("March");
+        monthList.add("April");
+        monthList.add("May");
+        monthList.add("June");
+        monthList.add("July");
+        monthList.add("August");
+        monthList.add("September");
+        monthList.add("October");
+        monthList.add("November");
+        monthList.add("December");
+        for (int i = 0; i < monthList.size(); i++) {
+            if (monthName.equals(monthList.get(i))) {
+                System.out.println(monthName + " is " + (i+1) + " month");
+            }
+        }
     }
-
 }
