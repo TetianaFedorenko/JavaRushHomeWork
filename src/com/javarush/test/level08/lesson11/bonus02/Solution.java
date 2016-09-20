f0d9db80968a08d134373871e35a531df5e10831
tetiana <test@test.com>
2016-09-20 16:32:42 +0300

    Bonus2_lesson11_level8

--- src/com/javarush/test/level08/lesson11/bonus02/Solution.java
+++ src/com/javarush/test/level08/lesson11/bonus02/Solution.java
@@ -3,8 +3,7 @@ package com.javarush.test.level08.lesson11.bonus02;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /* Нужно добавить в программу новую функциональность
 Задача: Программа определяет, какая семья (фамилию) живёт в доме с указанным номером.
@@ -28,24 +27,20 @@ public class Solution
     public static void main(String[] args) throws IOException
     {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        //list of addresses
-        List<String> addresses = new ArrayList<String>();
-        while (true)
-        {
-            String family = reader.readLine();
-            if (family.isEmpty()) break;
-
-            addresses.add(family);
+        HashMap<String,String> map = new HashMap<String, String>();
+        while (true){
+            String city= reader.readLine();
+            if (city.isEmpty()) break;
+            else {
+                String family = reader.readLine();
+                map.put(city,family);
+            }
+        }
+        String city1 = reader.readLine();
+        for (Map.Entry<String,String> pair : map.entrySet()){
+            if (pair.getKey().equals(city1))
+                System.out.println(pair.getValue());
         }
 
-        //read home number
-        int houseNumber = Integer.parseInt(reader.readLine());
-
-        if (0 <= houseNumber && houseNumber < addresses.size())
-        {
-            String familySecondName = addresses.get(houseNumber);
-            System.out.println(familySecondName);
-        }
     }
 }
